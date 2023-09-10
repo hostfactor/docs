a41c23cd36d2db57baabd7abb98ce911aeb1ea6c
edwardowens <eddiebeme@gmail.com>
2023-09-09 22:25:16 -0700

    feat: Fix blog edit url.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/hostfactor/docs/blog',
+          editUrl: 'https://github.com/hostfactor/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
