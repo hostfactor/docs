61942ba3e9fd6f51c46d93a202d871cd52811d15
edwardowens <eddiebeme@gmail.com>
2024-01-19 22:08:45 -0800

    feat: Adding subscription model revamp blog entry.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,8 +63,8 @@ const config = {
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'light',
-        disableSwitch: true,
+        defaultMode: 'dark',
+        // disableSwitch: true,
       },
       navbar: {
         title: 'Host Factor',
